38c825c876d29648de21d3cb068314aaedfbda7a
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-08-27 00:37:26 +0300

    feat: implement clearCart functionality in CartContainer and Context

--- src/CartContainer.jsx
+++ src/CartContainer.jsx
@@ -1,9 +1,8 @@
 import CartItem from './CartItem';
-import cartItems from './data';
 import { useGlobalContext } from './Context.jsx';
 
 const CartContainer = () => {
-	const { cart } = useGlobalContext();
+	const { cart, clearCart } = useGlobalContext();
 
 	const cartArray = Array.from(cart.entries());
 
@@ -39,10 +38,7 @@ const CartContainer = () => {
 						total <span>$10</span>
 					</h5>
 				</div>
-				<button
-					className="btn btn-hipster"
-					onClick={() => console.log('clear cart')}
-				>
+				<button className="btn btn-hipster" onClick={clearCart}>
 					clear cart
 				</button>
 			</footer>
